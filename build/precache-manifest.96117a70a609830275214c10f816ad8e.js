c43bd426ca59325b16fb1bbfe6f2b52435459309
Serghei Nuta <Serghei.Nuta@Nology.local>
2020-03-30 10:44:04 +0100

    comment git ignore

---
+++ build/precache-manifest.96117a70a609830275214c10f816ad8e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cbc0b7fac9f40a746b9d004d150cf4f",
+    "url": "/google-books-react/index.html"
+  },
+  {
+    "revision": "a7b3a835bdbb49943967",
+    "url": "/google-books-react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2c9a73114a205a3a8090",
+    "url": "/google-books-react/static/css/main.c8586907.chunk.css"
+  },
+  {
+    "revision": "a7b3a835bdbb49943967",
+    "url": "/google-books-react/static/js/2.a2150f46.chunk.js"
+  },
+  {
+    "revision": "d05926d49d122e0d92a31e315ba86c0c",
+    "url": "/google-books-react/static/js/2.a2150f46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c9a73114a205a3a8090",
+    "url": "/google-books-react/static/js/main.6eded6aa.chunk.js"
+  },
+  {
+    "revision": "7710c5b49e090a3108c8",
+    "url": "/google-books-react/static/js/runtime-main.fa503f16.js"
+  },
+  {
+    "revision": "13b28c484fde1b2fdd25288cbfc70a15",
+    "url": "/google-books-react/static/media/bookshelf.13b28c48.jpg"
+  }
+]);
